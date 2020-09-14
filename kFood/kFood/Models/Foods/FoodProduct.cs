e14a403b965740536846dd750e035c47fb74a26b
LaboSim <scierzniak96@gmail.com>
2020-09-14 21:02:20 +0200

    Add skelet of food product

---
+++ kFood/kFood/Models/Foods/FoodProduct.cs
@@ -0,0 +1,18 @@
+﻿namespace kFood.Models.Foods
+{
+    /// <summary>
+    /// The base model of food product
+    /// </summary>
+    public class FoodProduct
+    {
+        /// <summary>
+        /// The name of food product
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The photo of food in Base64
+        /// </summary>
+        public string FoodPhoto { get; set; }
+    }
+}
