dbf59ea7c4aeea1fed9f3845bc60379cf32ad162
LaboSim <scierzniak96@gmail.com>
2020-09-15 17:12:22 +0200

    Add annotations for Name property of food product model

--- kFood/kFood/Models/Foods/FoodProduct.cs
+++ kFood/kFood/Models/Foods/FoodProduct.cs
@@ -1,4 +1,7 @@
-﻿namespace kFood.Models.Foods
+﻿using kFood.ResourcesManager;
+using System.ComponentModel.DataAnnotations;
+
+namespace kFood.Models.Foods
 {
     /// <summary>
     /// The base model of food product
@@ -8,6 +11,8 @@
         /// <summary>
         /// The name of food product
         /// </summary>
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = nameof(Resources.FoodProductRequireError))]
+        [StringLength(255, MinimumLength = 3, ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = nameof(Resources.FoodProductNameError))]
         public string Name { get; set; }
 
         /// <summary>
