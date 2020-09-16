d740d0ca8a57a0827d866b3e86838689b4e6b150
LaboSim <scierzniak96@gmail.com>
2020-09-16 19:17:57 +0200

    Add skeleton of GetFoodProduct method in data access library

---
+++ kFood/DataAccessLibrary/FoodProductsDAO.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLibrary.Interfaces;
+using DataModelLibrary.Models.Foods;
+using System;
+
+namespace DataAccessLibrary
+{
+    public class FoodProductsDAO : IFoodProductsDAO
+    {
+        /// <summary>
+        /// Get specific food product
+        /// </summary>
+        /// <param name="foodId">The food product identifier</param>
+        /// <returns>The instance of <see cref="FoodProduct"/></returns>
+        public FoodProduct GetFoodProduct(int foodId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ kFood/DataAccessLibrary/Interfaces/IFoodProductsDAO.cs
@@ -0,0 +1,17 @@
+﻿using DataModelLibrary.Models.Foods;
+
+namespace DataAccessLibrary.Interfaces
+{
+    /// <summary>
+    /// The interface of food product data access
+    /// </summary>
+    public interface IFoodProductsDAO
+    {
+        /// <summary>
+        /// Get specific food product
+        /// </summary>
+        /// <param name="foodId">The food product identifier</param>
+        /// <returns>The instance of <see cref="FoodProduct"/></returns>
+        FoodProduct GetFoodProduct(int foodId);
+    }
+}
