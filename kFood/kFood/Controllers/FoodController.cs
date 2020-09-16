712bd74432e76db7b512864ae780f4af6a601274
LaboSim <scierzniak96@gmail.com>
2020-09-16 18:59:00 +0200

    Create GetFood method in FoodController

---
+++ kFood/kFood/Controllers/FoodController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace kFood.Controllers
+{
+    /// <summary>
+    /// The main controller for food operation
+    /// </summary>
+    public class FoodController : ApiController
+    {
+        /// <summary>
+        /// Get food product with details
+        /// </summary>
+        /// <param name="id">The food product identifier</param>
+        /// <returns></returns>
+        public IHttpActionResult GetFood(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
