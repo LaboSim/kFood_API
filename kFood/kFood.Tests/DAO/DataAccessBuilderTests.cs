7deb40843236a2730a50f72a1205cc9b891f6540
LaboSim <scierzniak96@gmail.com>
2020-09-20 16:40:42 +0200

    Added method to test getting connection string from web.Config

--- kFood/kFood.Tests/DAO/DataAccessBuilderTests.cs
+++ kFood/kFood.Tests/DAO/DataAccessBuilderTests.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using DataAccessLibrary;
+using DataAccessLibrary.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace kFood.Tests.DAO
 {
     public class DataAccessBuilderTests
     {
+        [Fact]
+        public void GetConnectionStringFromConfigFile_Success()
+        {
+            // Arange
+            string expectedMagicConnectionString = @"Server=SZYMON\BASE;Database=kFood_DEV;Integrated Security=SSPI";
+            IDataAccessBuilder dataAccessBuilder = new DataAccessBuilder();
+
+            // Act
+            string actualConnectionString = dataAccessBuilder.EstablishConnectionString();
+
+            // Assert
+            Assert.Equal(expectedMagicConnectionString, actualConnectionString);
+        }
     }
 }
