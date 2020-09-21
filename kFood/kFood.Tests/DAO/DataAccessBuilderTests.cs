a1c024981ede2064be253c3b734bc31bd0ebf101
LaboSim <scierzniak96@gmail.com>
2020-09-21 20:31:27 +0200

    Testing getting connection string from configuration file

--- kFood/kFood.Tests/DAO/DataAccessBuilderTests.cs
+++ kFood/kFood.Tests/DAO/DataAccessBuilderTests.cs
@@ -1,28 +1,31 @@
-﻿using DataAccessLibrary;
+﻿using Autofac.Extras.Moq;
+using DataAccessLibrary;
 using DataAccessLibrary.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 using Xunit;
 
 namespace kFood.Tests.DAO
 {
+    /// <summary>
+    /// The class for testing data access builder
+    /// </summary>
     public class DataAccessBuilderTests
     {
         [Fact]
         public void GetConnectionStringFromConfigFile_Success()
         {
-            // Arange
-            string expectedMagicConnectionString = @"Server=SZYMON\BASE;Database=kFood_DEV;Integrated Security=SSPI";
-            IDataAccessBuilder dataAccessBuilder = new DataAccessBuilder();
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<IDataAccessBuilder>()
+                    .Setup(x => x.EstablishConnectionString())
+                    .Returns(ConfigurationManager.ConnectionStrings["kFood_DEV"].ConnectionString);
 
-            // Act
-            string actualConnectionString = dataAccessBuilder.EstablishConnectionString();
+                var cls = mock.Create<DataAccessBuilder>();
+                string expectedMagicConnectionString = @"Server=SZYMON\BASE;Database=kFood_DEV;Integrated Security=SSPI;";
+                var actual = cls.EstablishConnectionString();
 
-            // Assert
-            Assert.Equal(expectedMagicConnectionString, actualConnectionString);
+                Assert.Equal(expectedMagicConnectionString, actual);
+            }
         }
     }
 }
