6f975306f86acb7b60e35c4c7edba61e38ae7a24
LaboSim <scierzniak96@gmail.com>
2020-10-13 20:53:06 +0200

    [Test] Get specific food product based on identifier

--- kFood/kFood.Tests/DAO/FoodProductsTests.cs
+++ kFood/kFood.Tests/DAO/FoodProductsTests.cs
@@ -1,15 +1,54 @@
 ﻿using Autofac.Extras.Moq;
 using DataAccessLibrary.Interfaces;
 using DataModelLibrary.Models.Foods;
-using kFood.Controllers;
-using System.Web.Http;
-using System.Web.Http.Results;
+using kFood.Models;
+using System;
 using Xunit;
 
 namespace kFood.Tests.DAO
 {
     public class FoodProductsTests
     {
-        
+        [Theory]
+        [InlineData(7)]
+        public void GetSpecificFoodProduct_Success(int foodId)
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                // Arange
+                mock.Mock<IFoodProductsDAO>()
+                    .Setup(x => x.GetFoodProduct(foodId))
+                    .Returns(GetSampleFoodProduct(foodId));
+
+                var cls = mock.Create<FoodProductProcessor>();
+                var expected = GetSampleFoodProduct(foodId);
+
+                // Act
+                var foodProduct = cls.GetSpecificFoodProduct(foodId);
+
+                // Assert
+                Assert.True(foodProduct != null);
+                Assert.Equal(expected.Id, foodProduct.Id);
+                Assert.Equal(expected.Name, foodProduct.Name);
+                Assert.Equal(expected.FoodImageURL, foodProduct.FoodImageURL);
+            }
+        }
+
+        #region Helper methods
+        /// <summary>
+        /// Get sample food product
+        /// </summary>
+        /// <param name="foodId">The fake identifier of food product</param>
+        /// <returns>The instance of <see cref="FoodProduct"/></returns>
+        private FoodProduct GetSampleFoodProduct(int foodId)
+        {
+            return new FoodProduct()
+            {
+                Id = foodId,
+                Name = "Sample food product",
+                FoodImageURL = new Uri("http://localhost:51052/view/foodproductimage/1")
+            };
+        } 
+        #endregion
     }
 }
