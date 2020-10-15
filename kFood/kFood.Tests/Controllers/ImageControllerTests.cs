78f3db4ddd2725b62a5763e9b368817f45520bf1
LaboSim <scierzniak96@gmail.com>
2020-10-15 19:24:03 +0200

    Initialize test of getting food product main image

--- kFood/kFood.Tests/Controllers/ImageControllerTests.cs
+++ kFood/kFood.Tests/Controllers/ImageControllerTests.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Autofac.Extras.Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace kFood.Tests.Controllers
 {
     public class ImageControllerTests
     {
+        [Theory]
+        [InlineData(4)]
+        public void GetMainImageForFoodProduct_Successful(int foodId)
+        {
+            using(var mock = AutoMock.GetLoose())
+            {
+
+            }
+        }
     }
 }
