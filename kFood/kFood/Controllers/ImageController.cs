d6e773c47e149b513d159089d758dad0dccc62a9
LaboSim <scierzniak96@gmail.com>
2020-10-18 14:58:14 +0200

    [Controller] Get food product main image

--- kFood/kFood/Controllers/ImageController.cs
+++ kFood/kFood/Controllers/ImageController.cs
@@ -1,13 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using kFood.Models;
+using kFood.Models.Interfaces;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace kFood.Controllers
 {
+    /// <summary>
+    /// Handle operation on images
+    /// </summary>
+    [RoutePrefix("view")]
     public class ImageController : ApiController
     {
+        #region Private Members
+        IImageProcessor _imageProcessor;
+        #endregion
+
+        #region Constructos
+        /// <summary>
+        /// The default constructor
+        /// </summary>
+        public ImageController()
+        {
+        }
+
+        /// <summary>
+        /// The parameterized constructor for unit tests
+        /// </summary>
+        /// <param name="imageProcessor">The injected instance of <see cref="IImageProcessor"/> to unit tests</param>
+        public ImageController(IImageProcessor imageProcessor)
+        {
+            this._imageProcessor = imageProcessor;
+        } 
+        #endregion
+
+        /// <summary>
+        /// Get food product main image
+        /// </summary>
+        /// <param name="id">The food product identifier</param>
+        /// <returns>The <see cref="ByteArrayContent"/> image</returns>
+        [HttpGet]
+        [Route("getImage/{id}")]
+        public IHttpActionResult GetFoodProductMainImage(int id)
+        {
+            _imageProcessor = _imageProcessor ?? new ImageProcessor();
+
+            IHttpActionResult response;
+            HttpResponseMessage responseMessage = new HttpResponseMessage(HttpStatusCode.OK);
+            responseMessage.Content = new ByteArrayContent(_imageProcessor.GetMainImageForSpecificFoodProduct(id));
+            responseMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("image/png");
+            response = ResponseMessage(responseMessage);
+            return response;
+        }
     }
 }
