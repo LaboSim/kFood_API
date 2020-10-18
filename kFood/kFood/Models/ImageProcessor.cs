9bb29c60fa657e9f6a122b151269f1739680a087
LaboSim <scierzniak96@gmail.com>
2020-10-18 15:23:28 +0200

    [Processor] Get food product main image

---
+++ kFood/kFood/Models/ImageProcessor.cs
@@ -0,0 +1,36 @@
+﻿using DataAccessLibrary;
+using DataAccessLibrary.Interfaces;
+using kFood.Models.Interfaces;
+
+namespace kFood.Models
+{
+    /// <summary>
+    /// The processor layer of image
+    /// </summary>
+    public class ImageProcessor : IImageProcessor
+    {
+        #region Private Members
+        IImageDAO _imageDAO;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Dedault constructor
+        /// </summary>
+        public ImageProcessor()
+        {
+        } 
+        #endregion
+
+        /// <summary>
+        /// Get food product main image
+        /// </summary>
+        /// <param name="foodId">The food product identifier</param>
+        /// <returns>The image as byte[]</returns>
+        public byte[] GetMainImageForSpecificFoodProduct(int foodId)
+        {
+            _imageDAO = _imageDAO = new ImageDAO();
+            return _imageDAO.GetFoodProductMainImage(foodId);
+        }
+    }
+}
--- kFood/kFood/Models/Interfaces/IImageProcessor.cs
+++ kFood/kFood/Models/Interfaces/IImageProcessor.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace kFood.Models.Interfaces
+﻿namespace kFood.Models.Interfaces
 {
+    /// <summary>
+    /// The interface of processor layer of image
+    /// </summary>
     public interface IImageProcessor
     {
+        /// <summary>
+        /// The signature of method to get food product main image
+        /// </summary>
+        /// <param name="foodId">The food product identifier</param>
+        /// <returns>The image as byte[]</returns>
         byte[] GetMainImageForSpecificFoodProduct(int foodId);
     }
 }
