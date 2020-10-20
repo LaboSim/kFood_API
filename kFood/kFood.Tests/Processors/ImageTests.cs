af5788de265fecfaefc4302b42259d619d1fddbe
LaboSim <scierzniak96@gmail.com>
2020-10-20 17:57:48 +0200

    [Test] 'GetFoodProductMain_Success' method to test getting food product main image from image processor

--- kFood/kFood.Tests/Processors/ImageTests.cs
+++ kFood/kFood.Tests/Processors/ImageTests.cs
@@ -1,12 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Autofac.Extras.Moq;
+using DataAccessLibrary.Interfaces;
+using kFood.Models;
+using Xunit;
 
 namespace kFood.Tests.Processors
 {
     public class ImageTests
     {
+        [Theory]
+        [InlineData(3)]
+        public void GetFoodProductMainImage_Success(int foodId)
+        {
+            using(var mock = AutoMock.GetLoose())
+            {
+                // Arange
+                mock.Mock<IImageDAO>()
+                    .Setup(x => x.GetFoodProductMainImage(foodId))
+                    .Returns(GetSampleByteImage());
+
+                var cls = mock.Create<ImageProcessor>();
+                var expected = GetSampleByteImage();
+
+                // Act
+                byte[] image = cls.GetMainImageForSpecificFoodProduct(foodId);
+
+                // Assert
+                Assert.True(image != null);
+                Assert.True(image.Length > 0);
+                Assert.Equal(expected, image);
+            }
+        }
+
+        #region Helper methods
+        /// <summary>
+        /// Get byte array imitate some image
+        /// </summary>
+        /// <returns>The byte array that imitate some image</returns>
+        private byte[] GetSampleByteImage()
+        {
+            return new byte[] { 255, 216 };
+        }
+
+        /// <summary>
+        /// Get empty byte array imitate lack of image
+        /// </summary>
+        /// <returns>The empty byte array which imitate lack of image</returns>
+        private byte[] GetEmptyByteImage()
+        {
+            return new byte[] { };
+        }
+        #endregion
     }
 }
