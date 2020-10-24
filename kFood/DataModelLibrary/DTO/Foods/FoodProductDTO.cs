be92ffc6911437ce8fc4eba098f876ee08fba7ba
LaboSim <scierzniak96@gmail.com>
2020-10-24 13:46:47 +0200

    [Model] Changed location of 'FoodProductDTO.cs'

--- kFood/DataModelLibrary/DTO/Foods/FoodProductDTO.cs
+++ kFood/DataModelLibrary/DTO/Foods/FoodProductDTO.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataModelLibrary.DTO
+namespace DataModelLibrary.DTO.Foods
 {
     public class FoodProductDTO
     {
