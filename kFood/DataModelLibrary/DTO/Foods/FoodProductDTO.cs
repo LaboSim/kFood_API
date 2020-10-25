a6e491b9b6dd2db0fc72d75c13c7aa9c0e047bc1
LaboSim <scierzniak96@gmail.com>
2020-10-25 12:10:00 +0100

    [Model] Created 'FoodProductDTO.cs'

--- kFood/DataModelLibrary/DTO/Foods/FoodProductDTO.cs
+++ kFood/DataModelLibrary/DTO/Foods/FoodProductDTO.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataModelLibrary.DTO.Foods
+﻿namespace DataModelLibrary.DTO.Foods
 {
     public class FoodProductDTO
     {
+        /// <summary>
+        /// The name of food product
+        /// </summary>
+        public string Name { get; set; }
     }
 }
