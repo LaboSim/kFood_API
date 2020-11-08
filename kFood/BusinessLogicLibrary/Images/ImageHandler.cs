4b474960c7b30b10218ecaca1a803ab3b3aed184
LaboSim <scierzniak96@gmail.com>
2020-11-08 23:26:17 +0100

    [BL] Added 'ImageHandler.cs' with interface

---
+++ kFood/BusinessLogicLibrary/Images/ImageHandler.cs
@@ -0,0 +1,13 @@
+﻿using BusinessLogicLibrary.Images.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLibrary.Images
+{
+    public class ImageHandler : IImageHandler
+    {
+    }
+}
