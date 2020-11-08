82d65a23e17425f2c34519b4acedbbb16248f356
LaboSim <scierzniak96@gmail.com>
2020-11-08 14:23:13 +0100

    [Model/Converters] Add new interface 'IImageConverter'

---
+++ kFood/DataModelLibrary/Conventers/Interfaces/IImageConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace DataModelLibrary.Conventers.Interfaces
+{
+    /// <summary>
+    /// The interface of image converter
+    /// </summary>
+    public interface IImageConverter
+    {
+        /// <summary>
+        /// The signature of method to convert image as BASE64 string to image
+        /// </summary>
+        /// <param name="base64Image">The image as BASE64 string</param>
+        /// <returns>The instance of <see cref="Image"/></returns>
+        Image ConvertToImage(string base64Image);
+    }
+}
