dcee6e85b8eec2fd721c9f61fcb2d3768e30944f
LaboSim <scierzniak96@gmail.com>
2020-11-08 13:54:57 +0100

    [Test] Testing conversion base64 image to Image (beginning)

--- kFood/kFood.Tests/Conventers/ImageConventerTests.cs
+++ kFood/kFood.Tests/Conventers/ImageConventerTests.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using Autofac.Extras.Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace kFood.Tests.Conventers
 {
-    class ImageConventerTests
+    public class ImageConventerTests
     {
+        [Theory]
+        [InlineData("R0lGODlhAQABAIAAAAAAAAAAACH5BAAAAAAALAAAAAABAAEAAAICTAEAOw==")]
+        public void ConvertBase64ToImage(string base64Photo)
+        {
+            // Arrange
+            IImageConverter imageConverter = new ImageConverter();
+
+            // Act
+            var image = imageConverter.ConvertToImage(base64Photo);
+
+            // Assert
+            Assert.True(image != null);
+        }
     }
 }
