7f89cffa34a9ab9754628f9d8cf1c580198e1c84
LaboSim <scierzniak96@gmail.com>
2020-11-08 21:12:13 +0100

    [Model/Conventers] Added 'ImageConeverter'

---
+++ kFood/DataModelLibrary/Conventers/ImageConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text.RegularExpressions;
+using DataModelLibrary.Conventers.Interfaces;
+
+namespace DataModelLibrary.Conventers
+{
+    /// <summary>
+    /// The image converter
+    /// </summary>
+    public class ImageConverter : IImageConverter
+    {
+        /// <summary>
+        /// Convert image as BASE64 string to image
+        /// </summary>
+        /// <param name="base64Image">The image as BASE64 string</param>
+        /// <returns>The instance of <see cref="Image"/></returns>
+        public Image ConvertToImage(string base64Image)
+        {
+            if (ValidateBase64(base64Image))
+            {
+                byte[] byteImage = Convert.FromBase64String(base64Image);
+
+                Image image;
+                using(MemoryStream ms = new MemoryStream(byteImage))
+                {
+                    image = Image.FromStream(ms);
+                }
+
+                return image;
+            }
+            else
+            {
+                return (Image)null;
+            }
+        }
+
+        /// <summary>
+        /// Validate whether input is BASE64 string
+        /// </summary>
+        /// <param name="base64Image">The BASE64 string</param>
+        /// <returns>True whether string is BASE64</returns>
+        private bool ValidateBase64(string base64Image)
+        {
+            string pattern = "^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$";
+            Regex regex = new Regex(pattern);
+
+            return regex.IsMatch(base64Image);
+        }
+    }
+}
