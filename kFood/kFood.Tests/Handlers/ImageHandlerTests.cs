099a1c6cc6487372e4995ee4f178bb409d831255
LaboSim <scierzniak96@gmail.com>
2020-11-11 15:09:15 +0100

    [Test] Create helper method to convert fake image from base64 to image

--- kFood/kFood.Tests/Handlers/ImageHandlerTests.cs
+++ kFood/kFood.Tests/Handlers/ImageHandlerTests.cs
@@ -1,12 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace kFood.Tests.Handlers
 {
-    class ImageHandlerTests
+    public class ImageHandlerTests
     {
+        [Theory]
+        [MemberData(nameof(GetSampleImage))]
+        public void SaveImage_Success(Image image)
+        {
+
+        }
+
+        #region Helper methods
+        /// <summary>
+        /// Get sample image instance
+        /// </summary>
+        /// <returns>The collection of fake instance <see cref="Image"/></returns>
+        public static IEnumerable<object[]> GetSampleImage()
+        {
+            return new List<object[]>
+            {
+                new object[]
+                {
+                    CreateSampleImage()
+                }
+            };
+        }
+
+        /// <summary>
+        /// Convert image as BASE64 string to image
+        /// </summary>
+        /// <returns>The instance of <see cref="Image"/></returns>
+        private static Image CreateSampleImage()
+        {
+            string base64Image = "R0lGODlhAQABAIAAAAAAAAAAACH5BAAAAAAALAAAAAABAAEAAAICTAEAOw=="; // this image is a single pixel (black)
+
+            byte[] byteImage = Convert.FromBase64String(base64Image);
+
+            Image image;
+            using(MemoryStream ms = new MemoryStream(byteImage))
+            {
+                image = Image.FromStream(ms);
+            }
+
+            return image;
+        }
+        #endregion
     }
 }
