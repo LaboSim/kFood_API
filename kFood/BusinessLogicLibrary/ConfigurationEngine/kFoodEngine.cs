103cfb8135523a4a95f9e0d59c700901db8a209f
LaboSim <scierzniak96@gmail.com>
2020-11-15 13:58:42 +0100

    [BL/Configuration] Created 'GetTemporaryPath'method to get directory for saving temporary image

--- kFood/BusinessLogicLibrary/ConfigurationEngine/kFoodEngine.cs
+++ kFood/BusinessLogicLibrary/ConfigurationEngine/kFoodEngine.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 
 namespace BusinessLogicLibrary.ConfigurationEngine
 {
+    /// <summary>
+    /// The kFoodEngine to handle app configuration
+    /// </summary>
     public class kFoodEngine
     {
+        /// <summary>
+        /// Get the path from app configuration to save a temporary file 
+        /// </summary>
+        /// <returns>The path to save a temporary image</returns>
+        /// <exception cref="NullReferenceException">Returned when cannot read web.Config for some reason</exception>
+        /// <exception cref="Exception">Returned when occurs some other exception than <see cref="NullReferenceException"/></exception>
+        public string GetTemporaryPath()
+        {
+            try
+            {
+                return ConfigurationManager.AppSettings["PathToTemporaryImage"];
+            }
+            catch(NullReferenceException ex)
+            {
+                throw new NullReferenceException(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
