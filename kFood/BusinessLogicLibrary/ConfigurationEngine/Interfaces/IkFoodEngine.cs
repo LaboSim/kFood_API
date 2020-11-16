1e5ab097dee005a63e416fc76c2f1675eba35129
LaboSim <scierzniak96@gmail.com>
2020-11-16 18:42:43 +0100

    [BL/Configuration] Added interface of 'kFoodEngine'

---
+++ kFood/BusinessLogicLibrary/ConfigurationEngine/Interfaces/IkFoodEngine.cs
@@ -0,0 +1,14 @@
+﻿namespace BusinessLogicLibrary.ConfigurationEngine.Interfaces
+{
+    /// <summary>
+    /// The interface of kFoodEngine application
+    /// </summary>
+    public interface IkFoodEngine
+    {
+        /// <summary>
+        /// The signature of method to get the path from app configuration to save a temporary file
+        /// </summary>
+        /// <returns>The path to save a temporary image</returns>
+        string GetTemporaryPath();
+    }
+}
