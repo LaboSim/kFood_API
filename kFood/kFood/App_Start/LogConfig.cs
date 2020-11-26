fc34fcb248bd47ab06ea2c5c25701b322c7229f5
LaboSim <scierzniak96@gmail.com>
2020-11-26 18:07:23 +0100

    [kFood] Place logger configuration to another class

---
+++ kFood/kFood/App_Start/LogConfig.cs
@@ -0,0 +1,20 @@
+﻿using Serilog;
+
+namespace kFood.App_Start
+{
+    /// <summary>
+    /// The log configuration
+    /// </summary>
+    public class LogConfig
+    {
+        /// <summary>
+        /// Configure logging
+        /// </summary>
+        public static void ConfigureLogging()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(@"D:\Szymon\Programowanie\Logs\testing.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+        }
+    }
+}
