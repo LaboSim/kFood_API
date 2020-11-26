60141075f5a4c4c771f7cfe34345a41f7fe1efcc
LaboSim <scierzniak96@gmail.com>
2020-11-26 20:50:15 +0100

    [Models/Messages] Added container of messages

---
+++ kFood/DataModelLibrary/Messages/MessageContainer.cs
@@ -0,0 +1,8 @@
+﻿namespace DataModelLibrary.Messages
+{
+    public static class MessageContainer
+    {
+        public const string StartAction = "Start action \"{actionName}\"";
+        public const string EndActionSuccess = "End action success: \"{actionName}\"";
+    }
+}
