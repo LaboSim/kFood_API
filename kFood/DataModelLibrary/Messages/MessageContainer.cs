b64b039903118fd8eb9f7bed18939a5c821f5faf
LaboSim <scierzniak96@gmail.com>
2020-11-30 18:35:39 +0100

    [Model/Messages] Added 'OutputActionJSON' const variable

--- kFood/DataModelLibrary/Messages/MessageContainer.cs
+++ kFood/DataModelLibrary/Messages/MessageContainer.cs
@@ -6,7 +6,11 @@
         public const string StartAction = "Start action \"{actionName}\"";
         public const string EndActionSuccess = "End action success: \"{actionName}\"";
         public const string EndActionNotFoundItem = "End action \"{actionName}\". Not found item for ID: \"{id}\"";
-        public const string EndActionError = "End action \"{actionName}\". Occured exception"; 
+        public const string EndActionError = "End action \"{actionName}\". Occured exception";
+        #endregion
+
+        #region Outputs
+        public const string OutputActionJSON = "OUTPUT: {outputJson}"; 
         #endregion
     }
 }
