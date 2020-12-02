8d0445e0b849e856e43bfe50d03c062abe94d018
LaboSim <scierzniak96@gmail.com>
2020-12-02 20:57:22 +0100

    [kFood] Modified LogConfig (log name and added SourceContext property)

--- kFood/kFood/App_Start/LogConfig.cs
+++ kFood/kFood/App_Start/LogConfig.cs
@@ -13,9 +13,9 @@ namespace kFood.App_Start
         public static void ConfigureLogging()
         {
             Log.Logger = new LoggerConfiguration()
-                .WriteTo.File(@"D:\Szymon\Programowanie\Logs\testing.txt", 
+                .WriteTo.File(@"D:\Szymon\Programowanie\Logs\kFoodAPI.txt", 
                     rollingInterval: RollingInterval.Day, 
-                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}")
                 .CreateLogger();
             Log.Logger.Information("*** START APPLICATION ***");
         }
