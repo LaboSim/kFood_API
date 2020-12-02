c158ae9651589605040ac2c8b30b3a1c0135f9d8
LaboSim <scierzniak96@gmail.com>
2020-12-02 21:01:16 +0100

    [Model/Messages] Removed hard source from messages template

--- kFood/DataModelLibrary/Messages/MessageContainer.cs
+++ kFood/DataModelLibrary/Messages/MessageContainer.cs
@@ -3,10 +3,10 @@
     public static class MessageContainer
     {
         #region Actions (controller)
-        public const string StartAction = "Src:{Source} -> Start action \"{actionName}\"";
-        public const string EndActionSuccess = "Src:{Source} -> End action success: \"{actionName}\"";
-        public const string EndActionNotFoundItem = "Src:{Source} -> End action \"{actionName}\". Not found item for ID: \"{id}\"";
-        public const string EndActionError = "Src:{Source} -> End action \"{actionName}\". Occured exception";
+        public const string StartAction = "Start action \"{actionName}\"";
+        public const string EndActionSuccess = "End action success: \"{actionName}\"";
+        public const string EndActionNotFoundItem = "End action \"{actionName}\". Not found item for ID: \"{id}\"";
+        public const string EndActionError = "End action \"{actionName}\". Occured exception";
         #endregion
 
         #region Outputs
