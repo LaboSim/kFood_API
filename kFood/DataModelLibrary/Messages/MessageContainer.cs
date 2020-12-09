6c03b79fdcd4c29be266792322f0d43a730d45dc
LaboSim <scierzniak96@gmail.com>
2020-12-09 18:44:32 +0100

    [Model/Messages] Added 'EmptyParam' message

--- kFood/DataModelLibrary/Messages/MessageContainer.cs
+++ kFood/DataModelLibrary/Messages/MessageContainer.cs
@@ -4,6 +4,7 @@
     {
         #region Actions (controller)
         public const string StartAction = "Start action \"{actionName}\"";
+        public const string EmptyParam = "Lack of required parameter";
         public const string EndActionSuccess = "End action success: \"{actionName}\"";
         public const string EndActionNotFoundItem = "End action \"{actionName}\". Not found item for ID: \"{id}\"";
         public const string EndActionError = "End action \"{actionName}\". Occured exception";
