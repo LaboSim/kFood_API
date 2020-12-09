ab1caaeaecaef853586154b9c37239eac159f115
LaboSim <scierzniak96@gmail.com>
2020-12-09 19:05:50 +0100

    [Model/Messages] Added 'EndActionResourceNotCreated' message

--- kFood/DataModelLibrary/Messages/MessageContainer.cs
+++ kFood/DataModelLibrary/Messages/MessageContainer.cs
@@ -8,6 +8,7 @@
 
         public const string EndActionSuccess = "End action success: \"{actionName}\"";
         public const string EndActionResourceCreated = "End action success: \"{actionName}\". Resource had been created";
+        public const string EndActionResourceNotCreated = "End action success: \"{actionName}\". Resource hadn't been created";
         public const string EndActionNotFoundItem = "End action \"{actionName}\". Not found item for ID: \"{id}\"";
         public const string EndActionError = "End action \"{actionName}\". Occured exception";
         #endregion
